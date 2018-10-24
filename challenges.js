b0cab9e9984d076d759a77e38b831814d4ba428c
DCI-PC-UBUNTU49 <ansumana.d@yahoocom>
2018-10-24 11:14:16 +0200

    master

---
+++ challenges.js
@@ -0,0 +1,14 @@
+/*Your Challenges
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score (GLOBAL score) when he rolls two 6 in a row. After that, it's the next player's turn.(Hint:  Always save the previous dice roll in a separate variable)
+
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in javascript. This is good opportunity to use google to figure this out :)
+
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+
+*/
+
+<!-- <input type="text" placeholder="Final Score" class="final-score" /> -->
+<img src="dice-5.png" alt="Dice" class="dice" id="dice-1" >
+<!-- <img src="dice-5.png" alt="Dice" class="dice" id="dice-2" > --> <!-- this is challenge task with two dices and also enable challenges.js file-->
